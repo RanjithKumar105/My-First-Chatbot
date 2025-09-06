911ce54db9d51ad50ea993758b03ca6b1e642873
RanjithKumar105 <ranjithkumar100506@gmail.com>
2025-09-05 18:49:51 -0700

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Ranjith",
+    author_email="ranjithsuvarna525@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
